2e4a0a6a4d830bebceb05fd00fa9453adbeb5faf
austinngan <angan20@stuy.edu>
2020-10-22 14:33:49 -0400

    Added test case for the third method

--- Tester.java
+++ Tester.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class Tester{
   public static void main(String[] args){
     int[] arr1={1,3,5,7,8,42};
+    int[][] arr2={{1,4,5,9},{9,0,2,1,0},{7,1,0,0}};
     System.out.println(ArrayOps.sum(arr1));
     System.out.println(ArrayOps.largest(arr1));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(arr2)));
   }
 }
