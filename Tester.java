13f0f0063e53dd03f01deff69963cc4613251dfb
austinngan <angan20@stuy.edu>
2020-10-22 14:22:07 -0400

    Added 1 test case for each method

---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+  public static void main(String[] args){
+    int[] arr1={1,3,5,7,8,42};
+    System.out.println(ArrayOps.sum(arr1));
+    System.out.println(ArrayOps.largest(arr1));
+  }
+}
