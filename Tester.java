af054b1acc1f69f568dfabe3cd359177630ef549
austinngan <angan20@stuy.edu>
2020-10-22 15:35:12 -0400

    Added test case for isRowMagic

--- Tester.java
+++ Tester.java
@@ -6,6 +6,7 @@ public class Tester{
     int[] arr1={1,3,5,7,8,42};
     int[][] arr2={{1,4,5,9},{9,0,2,1},{7,1,0,0}};
     int[][] arr3={{31,5,52,3},{2,4,6,7},{7,7,7,7,8},{1,1,3,5,4},{2}};
+    int[][] samearr={{1,2,3},{1,2,3},{1,2,3}};
     System.out.println(ArrayOps.sum(arr1)); //returns 66
     System.out.println(ArrayOps.sum(arr));  //returns 23
     System.out.println(ArrayOps.largest(arr1)); //returns 42
@@ -17,5 +18,6 @@ public class Tester{
     System.out.println(ArrayOps.sum(arr2)); //returns 39
     System.out.println(ArrayOps.sum(arr3)); //returns 162
     System.out.println(Arrays.toString(ArrayOps.sumCols(arr2)));
+    System.out.println(ArrayOps.isRowMagic(samearr));
   }
 }
