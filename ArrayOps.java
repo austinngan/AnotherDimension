2f472246d563b1c53d6856d7363fbf6df4a07d5d
austinngan <angan20@stuy.edu>
2020-10-26 18:01:44 -0400

    Made sameArray based off array length

--- ArrayOps.java
+++ ArrayOps.java
@@ -71,11 +71,12 @@ public class ArrayOps{
   }
   public static boolean sameArray(int[][]matrix,int[]arr){
     int count=0;
+    int len=matrix[1].length;
     if (ArrayOps.isRectangle(matrix)==false){
       return false;
     }
     else{
-      for (int i=0;i<matrix.length-1;i++){
+      for (int i=0;i<len-2;i++){
         if (arr[i]!=arr[i+1]){
           return false;
         }
@@ -83,7 +84,7 @@ public class ArrayOps{
           count+=1;
         }
       }
-      if (count==matrix.length-1){
+      if (count==len-2){
         return true;
       }
       else{
